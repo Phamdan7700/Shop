d5106c3d3029975a66c11697f17b631748bf5ad4
PhamDan <phamdan7700@gmail.com>
2021-10-24 21:00:00 +0700

    fix

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -52,27 +52,27 @@ export default function ProductPage({ data }: DetailProps) {
 }
 
 
-export const getStaticProps: GetStaticProps = async (context) => {
-    // Call an external API endpoint to get posts
-    const res = await fetch('https://fakestoreapi.com/products')
-    const data = await res.json()
+// export const getStaticProps: GetStaticProps = async (context) => {
+//     // Call an external API endpoint to get posts
+//     const res = await fetch('https://fakestoreapi.com/products')
+//     const data = await res.json()
 
-    // Get the paths we want to pre-render based on posts
-    const paths = data.map((item: Product) => ({
-        params: { slug: item.id },
-    }))
+//     // Get the paths we want to pre-render based on posts
+//     const paths = data.map((item: Product) => ({
+//         params: { slug: item.id },
+//     }))
 
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
-    return { paths, fallback: false }
-}
+//     // We'll pre-render only these paths at build time.
+//     // { fallback: false } means other routes should 404.
+//     return { paths, fallback: false }
+// }
 
-export const getStaticPaths: GetStaticPaths = async ({ params }) => {
-    // params contains the post `id`.
-    // If the route is like /posts/1, then params.id is 1
-    const res = await fetch(`https://fakestoreapi.com/products/${params?.slug}`)
-    const data = await res.json()
+// export const getStaticPaths: GetStaticPaths = async ({ params }) => {
+//     // params contains the post `id`.
+//     // If the route is like /posts/1, then params.id is 1
+//     const res = await fetch(`https://fakestoreapi.com/products/${params?.slug}`)
+//     const data = await res.json()
 
-    // Pass post data to the page via props
-    return { props: { data } }
-}
+//     // Pass post data to the page via props
+//     return { props: { data } }
+// }
