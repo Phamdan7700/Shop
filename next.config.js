f8c5cbf2e6241b3a065a6f6117d1581cc3f775a0
PhamDan <phamdan7700@gmail.com>
2021-12-12 22:17:30 +0700

    fix: img

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['fakestoreapi.com', 'images.unsplash.com', 'source.unsplash.com', '127.0.0.1'],
+    domains: ['fakestoreapi.com', 'images.unsplash.com', 'source.unsplash.com', '127.0.0.1', 'http://127.0.0.1:8000'],
   },
 
 }
